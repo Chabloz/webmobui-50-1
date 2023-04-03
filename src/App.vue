4320cac7128f5b998cc5b7b41de1e78c3bc124d6
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2023-04-03 15:38:01 +0200

    add watch and localStorage example

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script setup>
-import { computed, ref } from 'vue';
+  import { computed, ref, watch } from 'vue';
 
-  const x = ref(2);
+
+  const x = ref(localStorage.getItem('x') ?? 2);
   const y = computed(() => x.value * 2);
 
   function add() {
     x.value++;
   }
 
+  watch(x,() => localStorage.setItem('x', x.value))
+
 </script>
 
 <template>
+  <header>Test</header>
   <p> {{ x }}</p>
   <button @click="add()">+</button>
-  <input type="number" :value="x">
+  <input type="number" v-bind:value="x">
   <p> {{ y }}</p>
 </template>
 
 <style scoped>
-
+  header {
+    color: v-bind('x > 5 ? "red" : "blue"');
+  }
+  * {
+    display: block;
+  }
 </style>
