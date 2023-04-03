5cbe63a5d2196c4f9d752a0b0749d572fcd82966
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2023-04-03 14:37:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+  const x = ref(2);
+  const y = computed(() => x.value * 2);
+
+  function add() {
+    x.value++;
+  }
+
+</script>
+
+<template>
+  <p> {{ x }}</p>
+  <button @click="add()">+</button>
+  <input type="number" :value="x">
+  <p> {{ y }}</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// import { ref, computed } from 'vue';
+
+// const x = ref(2);
+// const y = computed(() => x.value * 2);
+// const z = computed(() => y.value * 2);
+// const ageMajority = 18;
+
+// console.log(x.value)
+// console.log(y.value)
+// console.log(z.value)
+// x.value = 4;
+// console.log(x.value)
+// console.log(y.value)
+// console.log(z.value)
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
