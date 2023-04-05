305eff2a158210432372ab5f6d11792d5a32cc8b
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2023-04-05 10:02:33 +0200

    example component web

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <script setup>
-  import { computed, ref, watch } from 'vue';
+  import TheHeader from './components/TheHeader.vue';
+  import TheMainPage from './components/TheMainPage.vue';
 
-
-  const x = ref(localStorage.getItem('x') ?? 2);
-  const y = computed(() => x.value * 2);
-
-  function add() {
-    x.value++;
-  }
-
-  watch(x,() => localStorage.setItem('x', x.value))
 
 </script>
 
 <template>
-  <header>Test</header>
-  <p> {{ x }}</p>
-  <button @click="add()">+</button>
-  <input type="number" v-bind:value="x">
-  <p> {{ y }}</p>
+  <the-header />
+  <the-main-page />
 </template>
 
 <style scoped>
-  header {
-    color: v-bind('x > 5 ? "red" : "blue"');
-  }
-  * {
-    display: block;
-  }
+
 </style>
